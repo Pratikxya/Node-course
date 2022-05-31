2534dbefd6153d676f40c27c033b49099f0157ec
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-05-31 21:13:53 +0545

    Printed in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,11 @@
 const getNotes = require("./notes.js");
 const validator = require("validator");
+const chalk = require("chalk");
 
 const msg = getNotes();
 console.log(msg);
 
 console.log(validator.isEmail("hello@gmail.com"));
+
+const greenMsg = chalk.green.inverse.bold("Success!");
+console.log(greenMsg);
