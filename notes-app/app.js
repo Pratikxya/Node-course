224afcb90638ae3080eda2ed4d169ac3aaf80254
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-05-31 18:26:39 +0545

    Imported useful file.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,3 @@
-const add = require("./utils.js");
-
-console.log(`Its working! ${add(4, -5)}`);
-
-// const fs = require("fs");
-
-// fs.writeFileSync("notes.txt", "My name is Pratikshya.");
-
-// fs.appendFileSync("notes.txt", " I am a learner.");
+const getNotes = require("./notes.js");
+const msg = getNotes();
+console.log(msg);
--- notes-app/utils.js
+++
@@ -1,9 +0,0 @@
-console.log("utils.js");
-
-// const name = "you";
-
-const add = function (a, b) {
-  return a + b;
-};
-
-module.exports = add;
