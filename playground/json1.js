95eb78ef9adda196b5fc61f88b2f393d3f2f94ca
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-06-06 17:43:06 +0545

    read some JSON file

--- playground/json1.js
+++ playground/json1.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday",
-};
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+// };
 
-const bookJSON = JSON.stringify(book);
-fs.writeFileSync("json1.json", bookJSON);
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync("json1.json", bookJSON);
+
+const dataBuffer = fs.readFileSync("json1.json");
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title);
