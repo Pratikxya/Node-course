27167e53806622ee4c253d67a19315063a942c7c
Pratikshya Poudel <pratikshyapoudel2000@gmail.com>
2022-06-06 17:52:54 +0545

    Working with JSON and the file system.

--- playground/json1.js
+++ playground/json1.js
@@ -1,3 +1,4 @@
+const { info } = require("console");
 const fs = require("fs");
 // const book = {
 //   title: "Ego is the Enemy",
@@ -7,7 +8,24 @@ const fs = require("fs");
 // const bookJSON = JSON.stringify(book);
 // fs.writeFileSync("json1.json", bookJSON);
 
+// const dataBuffer = fs.readFileSync("json1.json");
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
+
+//Challenge : Working with JSON and the file system
+
+// 1.Load and parse the JSON data
+// 2. Change the name1 and age property using your info
+// 3. Stringify the changed object and overwrite the original data
+// 4..Test your work by viewing the data in the JSON File.
+
 const dataBuffer = fs.readFileSync("json1.json");
 const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data.title);
+const user = JSON.parse(dataJSON);
+
+user.name = "Mike";
+user.age = 30;
+
+const userJSON = JSON.stringify(user);
+fs.writeFileSync("json1.json", userJSON);
